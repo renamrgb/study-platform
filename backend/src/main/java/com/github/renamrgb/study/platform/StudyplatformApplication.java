bbbf4a2e5008c42544cc415df5e755da693d460a
Renam Bulhoes <renamgustavo@live.com>
2022-10-17 20:57:20 -0300

    Iniciando projeto

---
+++ backend/src/main/java/com/github/renamrgb/study/platform/StudyplatformApplication.java
@@ -0,0 +1,12 @@
+package com.github.renamrgb.study.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyplatformApplication.class, args);
+	}
+}
